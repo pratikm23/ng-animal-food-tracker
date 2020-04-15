091d3ec1dbd1dc4b7d5d8762b668c890a9956d3e
Pratik <pratikm23@gmail.com>
2020-04-16 02:18:03 +0530

    Adding types

--- src/app/components/reporter/reporter.component.ts
+++ src/app/components/reporter/reporter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MealsService } from 'src/app/services/meals.service';
+import { Meal } from 'src/app/models/common.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reporter',
@@ -8,7 +10,7 @@ import { MealsService } from 'src/app/services/meals.service';
 })
 export class ReporterComponent implements OnInit {
   public columnDefs: any;
-  public rowData: any;
+  public rowData: Observable<Meal[]>;
 
   constructor(
     private mealsService: MealsService
