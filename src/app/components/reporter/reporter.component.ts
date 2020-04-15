350c068a39541465f37b95451775bbcd47484ced
Pratik <pratikm23@gmail.com>
2020-04-16 01:49:18 +0530

    Reporter module

--- src/app/components/reporter/reporter.component.ts
+++ src/app/components/reporter/reporter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MealsService } from 'src/app/services/meals.service';
 
 @Component({
   selector: 'app-reporter',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporter.component.css']
 })
 export class ReporterComponent implements OnInit {
+  public columnDefs: any;
+  public rowData: any;
 
-  constructor() { }
+  constructor(
+    private mealsService: MealsService
+  ) { }
 
   ngOnInit() {
+    this.columnDefs = [
+        {headerName: 'Park', field: 'park.name', sortable: true, filter: true},
+        {headerName: 'City', field: 'park.city', sortable: true, filter: true},
+        {headerName: 'Country', field: 'park.country', sortable: true, filter: true},
+        {headerName: 'Species', field: 'species.name', sortable: true, filter: true}, 
+        {headerName: 'Meal', field: 'food.name', sortable: true, filter: true},
+        {headerName: 'How much they ate?', field: 'food_quantity', sortable: true, filter: true},
+        {headerName: 'When did they eat?', field: 'time', sortable: true, filter: 'agDateColumnFilter'},
+        {headerName: 'How many of them ate?', field: 'species_quantity', sortable: true, filter: true},
+  ];
+    this.rowData = this.mealsService.getAllMeals();
   }
 
 }
